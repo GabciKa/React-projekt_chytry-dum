a878d55ad60efeea13705a9e9452c3f8ce860503
Gabriela Kudláčková <gabi@klabal.cz>
2021-10-31 10:31:13 +0100

    climate je kompletní

--- src/components/Climate/index.jsx
+++ src/components/Climate/index.jsx
@@ -1,20 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import  "./style.css";
 import tempIcon from "./img/temp.svg";
 
 const Climate = ({climate}) => {
+
+    const[temp, setTemp] = useState(climate.temperature);
+
     return (
         <div className="climate">
             <div className="climate__icon">
                 <img src={tempIcon} alt="temperature icon" />
             </div>
             <div className="climate__content">
-                <div className="climate__temperature">{climate.temperature}&deg;C</div>
+                <div className="climate__temperature">{temp}&deg;C</div>
                 <div className="climate__humidity">Vlhost vzduchu {climate.humidity}&nbsp;%</div>
             </div>
             <div className="climate__controls">
-                <button className="button">+</button>
-                <button className="button">-</button>
+                <button className="button" onClick={ () => { setTemp(temp+1) } }>+</button>
+                <button className="button" onClick={ () => { setTemp(temp-1)} }>-</button>
             </div>
         </div>
     )
